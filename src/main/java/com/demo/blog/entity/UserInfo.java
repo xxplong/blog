5fb554e2ff054d3d7c923897c5e47f87a041503d
Zengwl <983416510@qq.com>
2020-07-22 00:05:59 +0800

    springboot整合mybatis
    20200722
    springboot整合mybatis(30%)

---
+++ src/main/java/com/demo/blog/entity/UserInfo.java
@@ -0,0 +1,27 @@
+package com.demo.blog.entity;
+
+public class UserInfo {
+    private String username;
+    private int age;
+
+    public UserInfo(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
